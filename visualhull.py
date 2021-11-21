5ee3556576d0251a5c766f215bbef9c1b1a0b309
naruya <naruya.kondo@gmail.com>
2021-11-21 20:51:26 +0000

    clean visual hull

--- visualhull.py
+++ visualhull.py
@@ -1,5 +1,3 @@
-# python visualhull.py --config configs/demo --data_dir data/nerf_synthetic/lego --vh_test
-
 from absl import app
 from absl import flags
 from jax import config
@@ -9,6 +7,7 @@ from nerf import datasets
 import jax
 from jax import jit
 from jax import device_put
+from functools import partial
 import jax.numpy as jnp
 import os
 import cv2
@@ -17,158 +16,224 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-FLAGS = flags.FLAGS
-utils.define_flags()
-flags.DEFINE_string("voxel_dir", "data/voxel", "voxel data directory.")
-flags.DEFINE_string("vh_save", "shape", "Save type ('shape' of 'color')")
-flags.DEFINE_bool("vh_test", False, "If True, test the result of visual hull")
-config.parse_flags_with_absl()
-
-# larger size requires larger images
-t_n, t_f = FLAGS.near, FLAGS.far
-vsize = 400               # voxel size
-rsize = (t_f - t_n) / 2.  # real size
-# t_c = (t_f + t_n) / 2.  # center
-
-
-@jit
-def digitize(p):
+@partial(jit, static_argnums=(1,2,))
+def digitize(p, rsize, vsize):
     p = jnp.round((p+rsize) * (vsize/(rsize*2)))
     return jnp.clip(p.astype(jnp.int16), 0, vsize-1)
-    # requires more memory
-    # return jnp.digitize(p, jnp.linspace(t_n-t_c, t_f-t_c, vsize)).astype(jnp.int16)
+
+# somehow this requires more memory
+# @partial(jit, static_argnums=(1,2,3,))
+# def digitize(p, t_n, t_f, vsize):
+#   return jnp.digitize(p + (t_n + t_f) / 2., jnp.linspace(t_n, t_f, vsize-1))
 
 
-@jit
-def carve_voxel(o, d, mask):
+@partial(jit, static_argnums=(5,6,7,8,))
+def carve_voxel(o, d, mask, voxel_s, voxel_r, rsize, vsize, t_n, t_f):
+    # shape and ray counter
     voxel_si = jnp.zeros([vsize, vsize, vsize]).astype(jnp.uint8)
-    mask = device_put(mask)
+    voxel_ri = jnp.zeros([vsize, vsize, vsize]).astype(jnp.uint8)
 
     t_all = jnp.linspace(t_n, t_f, vsize+1)
-    ray_p = digitize(o[:,:,None] + d[:,:,None] * t_all[None, None, :, None])
+    ray_p = digitize(o[:,:,None] + d[:,:,None] * t_all[None, None, :, None], rsize, vsize)
 
+    # make a silhouette cone
     mask = jnp.repeat(mask[:,:,None].astype(jnp.uint8), vsize+1, axis=2)
-    voxel_si = voxel_si.at[ray_p[:,:,:,1], ray_p[:,:,:,0], ray_p[:,:,:,2]].set(mask)
-    return voxel_si
+    voxel_si = voxel_si.at[ray_p[:,:,:,0], ray_p[:,:,:,1], ray_p[:,:,:,2]].set(mask)
+    voxel_ri = voxel_si.at[ray_p[:,:,:,0], ray_p[:,:,:,1], ray_p[:,:,:,2]].set(jnp.ones_like(mask))
+    voxel_s = voxel_s + voxel_si
+    voxel_r = voxel_r + voxel_ri
+    return voxel_s, voxel_r
 
 
-@jit
-def paint_voxel(o, d, img, voxel_c, voxel_t, voxel_s):
+@partial(jit, static_argnums=(6,7,8,9,))
+def paint_voxel(o, d, img, voxel_c, voxel_t, voxel_s, rsize, vsize, t_n, t_f):
+    # color and closest distance
     voxel_ci = jnp.zeros([vsize, vsize, vsize, 3]).astype(jnp.float32)
     voxel_ti = jnp.zeros([vsize, vsize, vsize]).astype(jnp.int16) + (vsize+1)
 
-    t_all = jnp.linspace(t_n, t_f, vsize+1)    
-    ray_p = digitize(o[:,:,None] + d[:,:,None] * t_all[None, None, :, None])
+    t_all = jnp.linspace(t_n, t_f, vsize+1)
+    ray_p = digitize(o[:,:,None] + d[:,:,None] * t_all[None, None, :, None], rsize, vsize)
 
-    ti = jnp.argmax(voxel_s[ray_p[:,:,:,1], ray_p[:,:,:,0], ray_p[:,:,:,2]], axis=2)
+    # intersection of ray and voxel
+    ti = jnp.argmax(voxel_s[ray_p[:,:,:,0], ray_p[:,:,:,1], ray_p[:,:,:,2]], axis=2)
     ray_p_ti = jnp.sum((ray_p * jnp.eye(vsize+1)[ti][:,:,:,None]),axis=2).astype(jnp.int16)
-    voxel_ci = voxel_ci.at[ray_p_ti[:,:,1], ray_p_ti[:,:,0], ray_p_ti[:,:,2]].set(img)
-    voxel_ti = voxel_ti.at[ray_p_ti[:,:,1], ray_p_ti[:,:,0], ray_p_ti[:,:,2]].set(ti)
-    voxel_c = jnp.where((voxel_t > voxel_ti)[...,None], voxel_ci, voxel_c)
-    voxel_t = jnp.minimum(voxel_t, voxel_ti)
+    voxel_ci = voxel_ci.at[ray_p_ti[:,:,0], ray_p_ti[:,:,1], ray_p_ti[:,:,2]].set(img)
+    voxel_ti = voxel_ti.at[ray_p_ti[:,:,0], ray_p_ti[:,:,1], ray_p_ti[:,:,2]].set(ti)
+
+    # not update voxel_c at voxel_ti == 0
+    voxel_ci = jnp.where((voxel_ti == 0)[...,None], voxel_ci*0, voxel_ci)
+    voxel_ti = jnp.where(voxel_ti == 0, voxel_ti*0 + (vsize+1), voxel_ti)
+    voxel_c = jnp.where((voxel_ti < voxel_t)[...,None], voxel_ci, voxel_c)
+    voxel_t = jnp.minimum(voxel_ti, voxel_t)
     return voxel_t, voxel_c
 
 
-@jit
-def render_voxel(voxel_s, voxel_c, o, d):
+@partial(jit, static_argnums=(4,5,6,7,))
+def render_voxel(voxel_s, voxel_c, o, d, rsize, vsize, t_n, t_f):
     t_all = jnp.linspace(t_n, t_f, vsize+1)
-    ray_p = digitize(o[:,:,None] + d[:,:,None] * t_all[None, None, :, None])
+    ray_p = digitize(o[:,:,None] + d[:,:,None] * t_all[None, None, :, None], rsize, vsize)
 
-    ti = jnp.argmax(voxel_s[ray_p[:,:,:,1], ray_p[:,:,:,0], ray_p[:,:,:,2]], axis=2)
+    # intersection of ray and voxel
+    ti = jnp.argmax(voxel_s[ray_p[:,:,:,0], ray_p[:,:,:,1], ray_p[:,:,:,2]], axis=2)
     ray_p_ti = jnp.sum((ray_p * jnp.eye(vsize+1)[ti][:,:,:,None]),axis=2).astype(jnp.int16)
-    img = voxel_c[ray_p_ti[:,:,1], ray_p_ti[:,:,0], ray_p_ti[:,:,2]]
+    img = voxel_c[ray_p_ti[:,:,0], ray_p_ti[:,:,1], ray_p_ti[:,:,2]]
     return img
 
 
-class PureDataset(datasets.dataset_dict[FLAGS.dataset]):
-    def start(self):
-        pass
+def visualhull(FLAGS, dataset, test_dataset=None, target="", dilation=5, thresh=1.):
+    os.makedirs(os.path.join(FLAGS.voxel_dir+"_dil{}".format(dilation), target), exist_ok=True)
 
+    # larger size requires larger images
+    t_n, t_f = FLAGS.near, FLAGS.far
+    vsize = FLAGS.vsize
+    rsize = (t_f - t_n) / 2.  # real size
+    # t_c = (t_f + t_n) / 2.  # center
 
-def main(unused_argv):
-    if FLAGS.config is not None:
-        utils.update_flags(FLAGS)
-
-    target = FLAGS.data_dir.split("/")[-1]
-    os.makedirs(os.path.join(FLAGS.voxel_dir, target), exist_ok=True)
-
-    dataset = PureDataset("train", FLAGS)
-    dataset.images = dataset.images.reshape(-1,800,800,3)
-    dataset.rays = dataset.rays._replace(origins = dataset.rays.origins.reshape(-1,800,800,3))
-    dataset.rays = dataset.rays._replace(directions = dataset.rays.directions.reshape(-1,800,800,3))
-    dataset.rays = dataset.rays._replace(viewdirs = dataset.rays.viewdirs.reshape(-1,800,800,3))
-
-    # shape 
-    voxel_s = np.zeros([vsize, vsize, vsize]).astype(np.bool)
+    ### shape
+    voxel_s = device_put(jnp.zeros([vsize, vsize, vsize]).astype(jnp.int16))
+    voxel_r = device_put(jnp.zeros([vsize, vsize, vsize]).astype(jnp.float32))
 
     for idx in tqdm(range(dataset.size)):
         o = dataset.rays.origins[idx]
         d = dataset.rays.directions[idx]
         img = dataset.images[idx]
-        mask = np.sum(img != 1., axis=2) != 0
-        # remove whiteout
-        mask = cv2.morphologyEx(mask.astype(np.uint8), cv2.MORPH_CLOSE, np.ones((3,3)))
-        # dilation (It makes appearance worse, but recommended for voxel initialization)
-        mask = cv2.dilate(mask.astype(np.uint8), np.ones((3,3)), iterations=1)
-        voxel_s += carve_voxel(o, d, mask).block_until_ready()
 
-    voxel_s = (voxel_s == 100).astype(jnp.uint8)
+        if FLAGS.alpha_bkgd:
+            mask = img[Ellipsis, 3] > 0
+            img = img[Ellipsis, :3]
+        else:
+            # get silhouette and remove whiteout
+            mask = np.sum(img, axis=2) != 3
+            mask = cv2.morphologyEx(mask.astype(np.uint8), cv2.MORPH_CLOSE, np.ones((3,3)))
 
-    if FLAGS.vh_save == "shape":
-        print(voxel_s.dtype, voxel_s.shape)
-        np.save(os.path.join(FLAGS.voxel_dir, target, "voxel.npy"), voxel_s)
-        print("done!")
+        mask = cv2.dilate(mask.astype(np.uint8), np.ones((dilation,dilation)), iterations=1)
+        output = carve_voxel(o, d, mask, voxel_s, voxel_r, rsize, vsize, t_n, t_f)
+        jax.tree_map(lambda x: x.block_until_ready(), output)
+        voxel_s, voxel_r = output
+
+    voxel_s = ((voxel_s >= (voxel_r * thresh)) * (voxel_s > 0.)).astype(jnp.uint8)
+
+    np.save(os.path.join(FLAGS.voxel_dir+"_dil{}".format(dilation), target, "voxel.npy"), voxel_s)
+    print(voxel_s.dtype, voxel_s.shape, "\nshape done!")
+
+    ### color
+    voxel_c = device_put(jnp.zeros([vsize, vsize, vsize, 3]).astype(jnp.float32))
+    voxel_t = device_put(jnp.zeros([vsize, vsize, vsize]).astype(jnp.int16) + (vsize+1))
+
+    for idx in tqdm(range(dataset.size)):
+        o = dataset.rays.origins[idx]
+        d = dataset.rays.directions[idx]
+        img = dataset.images[idx, Ellipsis, :3]
+
+        output = paint_voxel(o, d, img, voxel_c, voxel_t, voxel_s, rsize, vsize, t_n, t_f)
+        jax.tree_map(lambda x: x.block_until_ready(), output)
+        voxel_t, voxel_c = output
+
+    np.save(os.path.join(FLAGS.voxel_dir+"_dil{}".format(dilation), target, "voxel_color.npy"), voxel_c)
+    print(voxel_c.dtype, voxel_c.shape, "\ncolor done!")
+
+    ### test
+    N=20
+    di = test_dataset.size // N
+    plt.figure(figsize=(200,40))
+    for i in range(N):
+        o = test_dataset.rays.origins[i*di]
+        d = test_dataset.rays.directions[i*di]
+        frame = render_voxel(voxel_s, voxel_c, o, d, rsize, vsize, t_n, t_f)
+        plt.subplot(6,N,i+1+N*0); plt.imshow(frame)
+
+    for i in range(N):
+        frame = test_dataset.images[i*di,Ellipsis,:3]
+        plt.subplot(6,N,i+1+N*1); plt.imshow(frame)
+
+    voxel_c_red = voxel_c*0 + jnp.array([1.,0.,0.]) * voxel_s[:,:,:,None]
+    pred_masks = []
+    for i in range(N):
+        o = test_dataset.rays.origins[i*di]
+        d = test_dataset.rays.directions[i*di]
+        frame = render_voxel(voxel_s, voxel_c_red, o, d, rsize, vsize, t_n, t_f)
+        pred_masks.append(frame)
+        plt.subplot(6,N,i+1+N*2); plt.imshow(frame)
+
+    masks = []
+    for i in range(N):
+        if FLAGS.alpha_bkgd:
+            mask = (test_dataset.images[i*di,Ellipsis,3:] > 0).astype(np.float32)
+        else:
+            mask = np.sum(test_dataset.images[i*di,Ellipsis,:3], axis=2) != 3
+            mask = (cv2.morphologyEx(mask.astype(np.uint8), cv2.MORPH_CLOSE, np.ones((3,3))))[:,:,None]
+        mask = mask * np.array([1.,0.,0.])
+        masks.append(mask)
+        plt.subplot(6,N,i+1+N*3); plt.imshow(mask)
+
+    for i in range(N):
+        plt.subplot(6,N,i+1+N*4); plt.imshow(np.clip(masks[i] - pred_masks[i], 0, 1))
+
+    for i in range(N):
+        plt.subplot(6,N,i+1+N*5); plt.imshow(np.clip(pred_masks[i] - masks[i], 0, 1))
+
+    plt.savefig(os.path.join(FLAGS.voxel_dir+"_dil{}".format(dilation), target, "voxel.png"))
+    # plt.show()
+    plt.close()
+
+    # import moviepy.editor as mpy
+    # frames = []
+    # for i in tqdm(range(test_dataset.size)):
+    #     o = test_dataset.rays.origins[i]
+    #     d = test_dataset.rays.directions[i]
+    #     frame = render_voxel(voxel_s, voxel_c, o, d, rsize, vsize, t_n, t_f).block_until_ready()
+    #     frames.append(frame)
+    # frames = [(np.array(frame) * 255.).astype(np.uint8) for frame in frames]
+    # clip = mpy.ImageSequenceClip(frames, fps=10)
+    # clip.write_gif(os.path.join(FLAGS.voxel_dir, target, "voxel.gif"))
+
+    print("test done!")
 
 
-    if FLAGS.vh_save == "color" or FLAGS.vh_test:
-        # color
-        voxel_c = device_put(jnp.ones([vsize, vsize, vsize, 3]).astype(jnp.float32))  # white
-        voxel_t = device_put(jnp.zeros([vsize, vsize, vsize]).astype(jnp.int16) + (vsize+1))
+def main(unused_argv):
+    utils.update_flags(FLAGS, no_nf=True)
 
-        for idx in tqdm(range(dataset.size)):
-            o = dataset.rays.origins[idx]
-            d = dataset.rays.directions[idx]
-            img = dataset.images[idx]
-            output = paint_voxel(o, d, img, voxel_c, voxel_t, voxel_s)
-            jax.tree_map(lambda x: x.block_until_ready(), output)
-            voxel_t, voxel_c = output
+    if FLAGS.alpha_bkgd:
+      FLAGS.num_rgb_channels = 4
+    else:
+      assert FLAGS.thresh < 1., "thresh < 1. is recommended"
 
-        if FLAGS.vh_save == "color":
-            print(voxel_c.dtype, voxel_c.shape)
-            np.save(os.path.join(FLAGS.voxel_dir, target, "voxel.npy"), voxel_c)
-            print("done!")
+    class PureDataset(datasets.dataset_dict[FLAGS.dataset]):
+        def start(self):
+            pass
 
+    target = FLAGS.data_dir.split("/")[-1]
 
-    if FLAGS.vh_test:
-        # test
-        N = 5
-        plt.figure(figsize=(40,8))
-        for i in range(N):
-            o = dataset.rays.origins[i]
-            d = dataset.rays.directions[i]
-            frame = render_voxel(voxel_s, voxel_c, o, d)
-            plt.subplot(1,N,i+1); plt.imshow(frame)
-        plt.savefig(os.path.join(FLAGS.voxel_dir, target, "voxel.png"))
-        plt.close()
+    dataset = PureDataset("train", FLAGS)
+    dataset.images = dataset.images.reshape(-1,800,800,FLAGS.num_rgb_channels)
+    dataset.rays = dataset.rays._replace(
+      origins=dataset.rays.origins.reshape(-1,800,800,3))
+    dataset.rays = dataset.rays._replace(
+      directions=dataset.rays.directions.reshape(-1,800,800,3))
+    dataset.rays = dataset.rays._replace(
+      viewdirs=dataset.rays.viewdirs.reshape(-1,800,800,3))
 
-        # import moviepy.editor as mpy
-        # test_dataset = PureDataset("test", FLAGS)
-        # test_dataset.images = test_dataset.images.reshape(-1,800,800,3)
-        # test_dataset.rays = test_dataset.rays._replace(origins = test_dataset.rays.origins.reshape(-1,800,800,3))
-        # test_dataset.rays = test_dataset.rays._replace(directions = test_dataset.rays.directions.reshape(-1,800,800,3))
-        # test_dataset.rays = test_dataset.rays._replace(viewdirs = test_dataset.rays.viewdirs.reshape(-1,800,800,3))
-        # frames = []
-        # for i in tqdm(range(test_dataset.size)):
-        #     o = test_dataset.rays.origins[i]
-        #     d = test_dataset.rays.directions[i]
-        #     frame = render_voxel(voxel_s, voxel_c, o, d).block_until_ready()
-        #     frames.append(frame)
-        # frames = [(np.array(frame) * 255.).astype(np.uint8) for frame in frames]
-        # clip = mpy.ImageSequenceClip(frames, fps=10)
-        # clip.write_gif(os.path.join(FLAGS.voxel_dir, target, "voxel.gif"))
+    test_dataset = PureDataset("test", FLAGS)
+    test_dataset.images = test_dataset.images.reshape(-1,800,800,FLAGS.num_rgb_channels)
+    test_dataset.rays = test_dataset.rays._replace(
+      origins=test_dataset.rays.origins.reshape(-1,800,800,3))
+    test_dataset.rays = test_dataset.rays._replace(
+      directions=test_dataset.rays.directions.reshape(-1,800,800,3))
+    test_dataset.rays = test_dataset.rays._replace(
+      viewdirs=test_dataset.rays.viewdirs.reshape(-1,800,800,3))
 
-        print("test done!")
+    if FLAGS.dataset == "nsvf":
+      utils.update_flags(FLAGS, no_nf=False)
+
+    visualhull(FLAGS, dataset, test_dataset, target, dilation=FLAGS.dilation, thresh=FLAGS.thresh)
 
 
 if __name__ == "__main__":
+    FLAGS = flags.FLAGS
+    utils.define_flags()
+    flags.DEFINE_integer("vsize", 400, "voxel size")
+    flags.DEFINE_string("voxel_dir", "data/voxel", "voxel data directory.")
+    flags.DEFINE_integer("dilation", 1, "dilation size")
+    flags.DEFINE_float("thresh", 1., "threshold")
+    config.parse_flags_with_absl()
     app.run(main)
