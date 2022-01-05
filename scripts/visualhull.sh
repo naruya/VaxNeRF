86a8bded905e9941ac3faa3a9887b8f629128415
naruya <naruya.kondo@gmail.com>
2022-01-05 14:35:32 +0000

    update visual hull script

--- scripts/visualhull.sh
+++ scripts/visualhull.sh
@@ -1,6 +1,4 @@
 DATA_ROOT=$1
-arg1=$2
-arg2=$3
 
 DATA_FOLDER="nerf_synthetic"
 SCENES="lego chair drums ficus hotdog materials mic ship"
@@ -8,12 +6,43 @@ SCENES="lego chair drums ficus hotdog materials mic ship"
 for scene in $SCENES; do
   echo $scene
   python visualhull.py \
-    --config configs/demo \
+    --config configs/blender \
     --data_dir "$DATA_ROOT"/"$DATA_FOLDER"/"$scene" \
     --voxel_dir "$DATA_ROOT"/voxel/"$scene" \
-    --dilation $arg1 \
-    --pooling $arg2 \
+    --dilation 7 \
+    --pooling 7 \
     --thresh 1. \
     --alpha_bkgd \
+    --test
+done
+
+
+DATA_FOLDER="Synthetic_NSVF"
+SCENES="Bike Palace Robot Toad Wineholder"
+
+for scene in $SCENES; do
+  echo $scene
+  python visualhull.py \
+    --config configs/nsvf \
+    --data_dir "$DATA_ROOT"/"$DATA_FOLDER"/"$scene" \
+    --voxel_dir "$DATA_ROOT"/voxel/"$scene" \
+    --dilation 11 \
+    --pooling 7 \
+    --thresh 1. \
+    --alpha_bkgd \
+    --test
+done
+
+SCENES="Lifestyle Spaceship Steamtrain"
+
+for scene in $SCENES; do
+  echo $scene
+  python visualhull.py \
+    --config configs/nsvf \
+    --data_dir "$DATA_ROOT"/"$DATA_FOLDER"/"$scene" \
+    --voxel_dir "$DATA_ROOT"/voxel/"$scene" \
+    --dilation 11 \
+    --pooling 7 \
+    --thresh 0.9 \
     --test
 done
